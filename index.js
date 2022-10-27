5cb674bf34b3d6dd3db8c574e92a9b48b9b49cec
Pocevicius <lukas.pocevicius@gmail.com>
2022-10-27 18:32:36 +0300

    refactored

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
-var bodyParser = require("body-parser");
-
+const bodyParser = require("body-parser");
+const tasksRoutes = require("./api/routes/tasks");
 const app = express();
-//var cors=require("cors")
+
+// var cors = require("cors");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//app.use(cors())
-const tasks = [];
+// app.use(cors());
+
+app.use(tasksRoutes);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,33 +19,6 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
-});
-
-app.get("/getTasks", function (req, res) {
-  res.status(200).json({ tasks: tasks });
-});
-
-app.post("/insertTask", function (req, res) {
-  console.log(req.body.task);
-  tasks.push({
-    id: req.body.id,
-    task: req.body.task,
-    isDone: req.body.isDone,
-  });
-
-  res.status(200).json({ statusMessage: "task was inserted successfully" });
-});
-
-app.put("/editTask/:id", function (req, res) {
-  // console.log(req.params.id);
-  // console.log(req.body.editedTask);
-
-  const index = tasks.findIndex((task) => task.id === req.params.id);
-  tasks[index].task = req.body.editedTask;
-
-  console.log("task", tasks);
-
-  res.status(200).json({ statusMessage: "task was edited successfully" });
 });
 
 app.listen(3000);
